869b7f44e3e769d512a65eae6d231bfc27b14032
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2022-06-30 17:11:24 -0700

    Misc TypeScript conversions (#67967)
    About The Pull Request
    
    An atomization of #67809.
    
    This PR aims to convert some miscellaneous UIs into TypeScript. Some of these are low hanging fruit, don't hate me, I am working up the self hatred to do more.
    Why It's Good For The Game
    
    TypeScript is objectively better and provides great tools to debug code. We should really work to just convert all the UI into TSX
    Changelog
    
    cl
    code: Refactored a large number of TGUI interfaces into TypeScript. If something's broken, report it!
    /cl

--- tgui/packages/tgui/interfaces/NtosRequestKiosk.js
+++ tgui/packages/tgui/interfaces/NtosRequestKiosk.js
@@ -1,7 +1,7 @@
 import { RequestKioskContent } from './RequestKiosk';
 import { NtosWindow } from '../layouts';
 
-export const NtosRequestKiosk = (props, context) => {
+export const NtosBountyBoard = () => {
   return (
     <NtosWindow width={550} height={600}>
       <NtosWindow.Content scrollable>

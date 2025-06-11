HolyTigerGod/Voidcrew

Misc TypeScript conversions (#67967)
About The Pull Request

An atomization of #67809.

This PR aims to convert some miscellaneous UIs into TypeScript. Some of these are low hanging fruit, don't hate me, I am working up the self hatred to do more.
Why It's Good For The Game

TypeScript is objectively better and provides great tools to debug code. We should really work to just convert all the UI into TSX
Changelog

cl
code: Refactored a large number of TGUI interfaces into TypeScript. If something's broken, report it!
/cl

tgui/packages/tgui/interfaces/Aquarium.tsx
@@ -1,0 +1,85 @@
+import { BooleanLike } from 'common/react';
+import { useBackend } from '../backend';
+import { Button, Flex, Knob, LabeledControls, Section } from '../components';
+import { Window } from '../layouts';
+
+type Data = {
+  temperature: number;
+  fluid_type: string;
+  minTemperature: number;
+  maxTemperature: number;
+  fluidTypes: string[];
+  contents: { ref: string; name: string }[];
+  allow_breeding: BooleanLike;
+};
+
+export const Aquarium = (props, context) => {
+  const { act, data } = useBackend<Data>(context);
+  const {
+    temperature,
+    fluid_type,
+    minTemperature,
+    maxTemperature,
+    fluidTypes,
+    contents,
+    allow_breeding,
+  } = data;
+
+  return (
+    <Window width={500} height={400}>
+      <Window.Content>
+        <Section title="Aquarium Controls">
+          <LabeledControls>
+            <LabeledControls.Item label="Temperature">
+              <Knob
+                size={1.25}
+                mb={1}
+                value={temperature}
+                unit="K"
+                minValue={minTemperature}
+                maxValue={maxTemperature}
+                step={1}
+                stepPixelSize={1}
+                onDrag={(_, value) =>
+                  act('temperature', {
+                    temperature: value,
+                  })
+                }
+              />
+            </LabeledControls.Item>
+            <LabeledControls.Item label="Fluid">
+              <Flex direction="column" mb={1}>
+                {fluidTypes.map((f) => (
+                  <Flex.Item key={f}>
+                    <Button
+                      fluid
+                      content={f}
+                      selected={fluid_type === f}
+                      onClick={() => act('fluid', { fluid: f })}
+                    />
+                  </Flex.Item>
+                ))}
+              </Flex>
+            </LabeledControls.Item>
+            <LabeledControls.Item label="Reproduction Prevention System">
+              <Button
+                content={allow_breeding ? 'Offline' : 'Online'}
+                selected={!allow_breeding}
+                onClick={() => act('allow_breeding')}
+              />
+            </LabeledControls.Item>
+          </LabeledControls>
+        </Section>
+        <Section title="Contents">
+          {contents.map((movable) => (
+            <Button
+              key={movable.ref}
+              content={movable.name}
+              onClick={() => act('remove', { ref: movable.ref })}
+            />
+          ))}
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};

Metekillot/TekquiemSS13

tgui/packages/tgui/interfaces/Aquarium.tsx
@@ -1,0 +1,84 @@
+import { BooleanLike } from 'common/react';
+import { useBackend } from '../backend';
+import { Button, Dropdown, Knob, LabeledControls, Section } from '../components';
+import { Window } from '../layouts';
+
+type Data = {
+  temperature: number;
+  fluid_type: string;
+  minTemperature: number;
+  maxTemperature: number;
+  fluidTypes: string[];
+  contents: { ref: string; name: string }[];
+  allow_breeding: BooleanLike;
+};
+
+export const Aquarium = (props, context) => {
+  const { act, data } = useBackend<Data>(context);
+  const {
+    temperature,
+    fluid_type,
+    minTemperature,
+    maxTemperature,
+    fluidTypes,
+    contents,
+  } = data;
+
+  return (
+    <Window width={500} height={400}>
+      <Window.Content>
+        <Section title="Aquarium Controls">
+          <LabeledControls>
+            <LabeledControls.Item label="Temperature">
+              <Knob
+                size={1.25}
+                mb={1}
+                value={temperature}
+                unit="K"
+                minValue={minTemperature}
+                maxValue={maxTemperature}
+                step={1}
+                stepPixelSize={1}
+                onDrag={(_, value) =>
+                  act('temperature', {
+                    temperature: value,
+                  })
+                }
+              />
+            </LabeledControls.Item>
+            <LabeledControls.Item label="Fluid">
+              <Flex direction="column" mb={1}>
+                {fluidTypes.map((f) => (
+                  <Flex.Item key={f}>
+                    <Button
+                      fluid
+                      content={f}
+                      selected={fluid_type === f}
+                      onClick={() => act('fluid', { fluid: f })}
+                    />
+                  </Flex.Item>
+                ))}
+              </Flex>
+            </LabeledControls.Item>
+            <LabeledControls.Item label="Reproduction Prevention System">
+              <Button
+                content={allow_breeding ? 'Offline' : 'Online'}
+                selected={!allow_breeding}
+                onClick={() => act('allow_breeding')}
+              />
+            </LabeledControls.Item>
+          </LabeledControls>
+        </Section>
+        <Section title="Contents">
+          {contents.map((movable) => (
+            <Button
+              key={movable.ref}
+              content={movable.name}
+              onClick={() => act('remove', { ref: movable.ref })}
+            />
+          ))}
+        </Section>
+      </Window.Content>
+    </Window>
+  );
+};