84efc7da8af52e553190b516c7d04a9c4f7ee9b2
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2023-12-14 23:02:19 -0800

    Replaces babel with swc (#80310)
    Revival of #79943. I was not done with Babel. For some reason, swc was
    running very high memory. It was the minifier. If we use our current,
    only swapping out babel (the previous PR swapped the minifier AND
    transpiler), we not only keep the memory usage down to current levels
    but we **slash** TGUI build times. Look at these numbers!
    
    <details>
    <summary>Test environment / My specs</summary>
    
    intel i7-13700kf
    32GB RAM
    windows 11
    intel 660p m.2 SSD
    asus 4080 TUF series
    
    </details>
    
    <details>
    <summary>Benchmarks</summary>
    
    everything in the tgui/.yarn/webpack folder and all files in tgui/public
    **EXCEPT** tgui.html and tgui-polyfill.min.js
    
    Babel is our CURRENT.
    
    Build time
    
    ![2](https://github.com/tgstation/tgstation/assets/42397676/05324a54-2230-4173-a443-ef2aaa82e034)
    SWC is 64.36% faster than our current solution
    
    Peak memory usage
    
    ![1](https://github.com/tgstation/tgstation/assets/42397676/ab84a91d-7e48-43f8-be1b-7b7b9b36f30a)
    
    </details>
    
    What's the catch? Do we need to use node_modules now? No!
    
    Faster build tools!
    N/A nothing player facing

--- tgui/webpack.config.js
+++ tgui/webpack.config.js
@@ -7,7 +7,6 @@
 const webpack = require('webpack');
 const path = require('path');
 const ExtractCssPlugin = require('mini-css-extract-plugin');
-const { createBabelConfig } = require('./babel.config.js');
 
 const createStats = (verbose) => ({
   assets: verbose,
@@ -51,11 +50,10 @@ module.exports = (env = {}, argv) => {
     module: {
       rules: [
         {
-          test: /\.(js(x)?|cjs|ts(x)?)$/,
+          test: /\.([tj]s(x)?|cjs)$/,
           use: [
             {
-              loader: 'babel-loader',
-              options: createBabelConfig({ mode }),
+              loader: require.resolve('swc-loader'),
             },
           ],
         },
