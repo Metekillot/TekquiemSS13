1949411d272d25c9fdaa31cc75839d8c9d724eab
Jeremiah <42397676+jlsnow301@users.noreply.github.com>
2023-12-17 21:35:41 -0800

    Typescript image component (#80291)
    ## About The Pull Request
    No longer rely on Box to create images. A pretty simple change for most
    UIs. Solves a kinda weird solution for image rendering we had in Box.
    
    Did some small refactors along the way. Emojipedia is in TS now.
    ## Why It's Good For The Game
    Bug fix
    Fixes #80282
    ## Changelog
    :cl:
    fix: Emojipedia shouldn't bluescreen anymore.
    /:cl:

--- tgui/packages/tgui/components/BodyZoneSelector.tsx
+++ tgui/packages/tgui/components/BodyZoneSelector.tsx
@@ -1,6 +1,6 @@
 import { Component, createRef } from 'react';
 import { resolveAsset } from '../assets';
-import { Box } from './Box';
+import { Image } from './Image';
 
 export enum BodyZone {
   Head = 'head',
@@ -85,9 +85,8 @@ export class BodyZoneSelector extends Component<
           position: 'relative',
         }}
       >
-        <Box
-          as="img"
-          src={resolveAsset('body_zones.base.png')}
+        <Image
+          src={resolveAsset(`body_zones.base_${theme}.png`)}
           onClick={() => {
             const onClick = this.props.onClick;
             if (onClick && this.state.hoverZone) {
@@ -119,8 +118,7 @@ export class BodyZoneSelector extends Component<
         />
 
         {selectedZone && (
-          <Box
-            as="img"
+          <Image
             src={resolveAsset(`body_zones.${selectedZone}.png`)}
             style={{
               pointerEvents: 'none',
@@ -132,8 +130,7 @@ export class BodyZoneSelector extends Component<
         )}
 
         {hoverZone && hoverZone !== selectedZone && (
-          <Box
-            as="img"
+          <Image
             src={resolveAsset(`body_zones.${hoverZone}.png`)}
             style={{
               opacity: 0.5,
