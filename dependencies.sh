bf207a04772994b2e5dedbc1329b949c492df6ce
Joshua Kidder <anticitizenthree@gmail.com>
2025-06-17 23:47:33 -0400

    slam their startup directory onto ours

--- dependencies.sh
+++ dependencies.sh
@@ -3,23 +3,21 @@
 #Project dependencies file
 #Final authority on what's required to fully build the project
 
-# byond version
+# byond version. Also must be updated in .tgs.yml
 export BYOND_MAJOR=516
 export BYOND_MINOR=1659
 
 #rust_g git tag
-export RUST_G_VERSION=3.8.0
+export RUST_G_VERSION=3.9.0
 
-#node version
-export NODE_VERSION=22
-export NODE_VERSION_PRECISE=22.14.0
-export NODE_VERSION_LTS=22.14.0
+# node version
+export NODE_VERSION_LTS=22.11.0
+
+# Bun version
+export BUN_VERSION=1.2.16
 
 # SpacemanDMM git tag
-export SPACEMAN_DMM_VERSION=suite-1.9
-
-# Extools git tag
-export EXTOOLS_VERSION=v0.0.7
+export SPACEMAN_DMM_VERSION=suite-1.10
 
 # Python version for mapmerge and other tools
 export PYTHON_VERSION=3.9.0
@@ -35,3 +33,4 @@ export CUTTER_REPO=spacestation13/hypnagogic
 
 #hypnagogic git tag
 export CUTTER_VERSION=v4.0.0
+
